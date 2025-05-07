f802cf533b006ed14e25ed7da6639f9c0efe0511
emolatt <165890962+emolatt@users.noreply.github.com>
2025-05-07 17:30:43 +0200

    restructured files

--- main.go
+++ main.go
@@ -2,116 +2,40 @@ package main
 
 import (
 	"bytes"
-	"errors"
-	"io"
-
+	"fmt"
+	"github.com/emolatt/xk6-parquet/pkg/source"
 	"github.com/xitongsys/parquet-go/source"
-	"github.com/xitongsys/parquet-go/reader"
-	"go.k6.io/k6/js/common"
 	"go.k6.io/k6/js/modules"
-	"go.k6.io/k6/js/modules/k6"
-
-	"github.com/dop251/goja"
 )
 
-func init() {
-	modules.Register("k6/x/parquet", New())
-}
-
-// Parquet is the k6 extension module
-type Parquet struct{}
-
-// Ensure Parquet implements the modules.Instance interface
-var _ modules.Instance = &Parquet{}
-
-// New returns a new instance of the parquet module
-func New() modules.Instance {
-	return &Parquet{}
-}
-
-// Exports returns the exported functions from the module
-func (p *Parquet) Exports() modules.Exports {
-	return modules.Exports{
-		Default: &ParquetModule{},
-	}
-}
-
-// ParquetModule contains the actual functionality exposed to JS
-type ParquetModule struct {
-	vu modules.VU
-}
-
-func (p *ParquetModule) SetVU(vu modules.VU) {
-	p.vu = vu
-}
-
-func (p *ParquetModule) Reset() {}
-
-func (p *ParquetModule) ReadParquetBytes(data goja.Value) goja.Value {
-	rt := p.vu.Runtime()
-
-	// Convert the input Uint8Array to a Go []byte
-	jsBuf := data.ToObject(rt)
-	length := jsBuf.Get("length").ToInteger()
-	goBuf := make([]byte, length)
-	for i := int64(0); i < length; i++ {
-		goBuf[i] = byte(jsBuf.Get(i).ToInteger())
-	}
-
-	reader := &MemoryFileReader{
-		buf: bytes.NewBuffer(goBuf),
-	}
-
-	pr, err := reader.NewParquetReader()
-	if err != nil {
-		common.Throw(rt, err)
-	}
-
-	num := int(pr.GetNumRows())
-	res := make([]map[string]interface{}, num)
-
-	if err := pr.Read(&res); err != nil {
-		common.Throw(rt, err)
-	}
-
-	pr.ReadStop()
-	return rt.ToValue(res)
-}
-
-// MemoryFileReader implements the source.ParquetFile interface from parquet-go,
-// wrapping a byte buffer so we can read from in-memory data
+// MemoryFileReader is a custom file reader to read Parquet data from memory (e.g., Uint8Array)
 type MemoryFileReader struct {
-	buf *bytes.Buffer
+	buf []byte
 }
 
-func (m *MemoryFileReader) Create(name string) (source.ParquetFile, error) {
-	return nil, errors.New("not implemented")
+// New creates a new MemoryFileReader from the byte array
+func NewMemoryFileReader(data []byte) *MemoryFileReader {
+	return &MemoryFileReader{buf: data}
 }
 
-func (m *MemoryFileReader) Open(name string) (source.ParquetFile, error) {
-	return nil, errors.New("not implemented")
+// Read reads from the memory buffer
+func (m *MemoryFileReader) Read(p []byte) (n int, err error) {
+	copy(p, m.buf)
+	return len(m.buf), nil
 }
 
-func (m *MemoryFileReader) Seek(offset int64, whence int) (int64, error) {
-	return m.buf.Seek(offset, whence)
+// Create is a placeholder method to implement the ParquetFile interface
+func (m *MemoryFileReader) Create(path string) (source.ParquetFile, error) {
+	return m, nil
 }
 
-func (m *MemoryFileReader) Read(p []byte) (int, error) {
-	return m.buf.Read(p)
+// Write is a placeholder method to implement the ParquetFile interface
+func (m *MemoryFileReader) Write(p []byte) (n int, err error) {
+	// Not implemented
+	return 0, fmt.Errorf("Write method not implemented")
 }
 
-func (m *MemoryFileReader) Write(p []byte) (int, error) {
-	return 0, errors.New("write not supported")
-}
-
-func (m *MemoryFileReader) Close() error {
-	return nil
-}
-
-func (m *MemoryFileReader) Name() string {
-	return "memory"
-}
-
-func (m *MemoryFileReader) NewParquetReader() (*reader.ParquetReader, error) {
-	return reader.NewParquetReader(m, nil, int64(4))
+// init registers the module with k6
+func init() {
+	modules.Register("k6/x/parquet", New)
 }
